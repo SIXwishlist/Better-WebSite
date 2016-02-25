12d0d5e0a4a8806027273716f02ee38ceb7454ba
Thomas Wilbur <dodiaraculus@outlook.com>
2016-02-25 17:28:47 +0000

    installer

--- install/index.php
+++ install/index.php
@@ -1,28 +1,13 @@
-<?php 
-
-function errorHandler($n, $m, $f, $l) {
-    require_once 'errorsend.php';
-
-$config = array(
-    // required
-    'access_token' => 'a4073d551fb44087b5ebeea61588bbc4',
-    // optional - environment name. any string will do.
-    'environment' => 'production',
-    // optional - path to directory your code is in. used for linking stack traces.
-    'root' => $_SERVER['DOCUMENT_ROOT']
-);
-Rollbar::init($config);
-Rollbar::report_message($m, 'error');
-  $str = 'phperror.php?n=' . urlencode($n) . '&m=' . urlencode($m) . '&f=' . urlencode($f) . '&l=' . urlencode($l);
-    header('Location: ' . $str);
+<?php
+session_start();
+//Secure Code
+if (file_exists('install.lock') && !isset($_SESSION['install_lock'])) {
+    echo 'The install.lock file exists, please delete it to reinstall';
+    die();
+} else {
+    file_put_contents('install.lock', '');
+    $_SESSION['install_lock'] = 'unlocked';
 }
-
-set_error_handler('errorHandler');
-$neededpartner = false;
-if (!isset($testmode))
-{
-    session_start();
-error_reporting(0);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,237 +15,124 @@ error_reporting(0);
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Install Tecflare Multisite</title>
-    <link href="style/bootstrap.min.css" rel="stylesheet">
-    <link href="style/bootstrap.theme.css" rel="stylesheet">
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <title>poKe installer</title>
+ 
+    <link href="css/bootstrap.css" rel="stylesheet">
+ <link href="css/metro.css" rel="stylesheet">
+<script src="js/metro.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.2.0/zxcvbn.js"></script>
+
   </head>
-  <body class="container">
+  <body>
    
-   <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
 
-      <a class="navbar-brand"><strong>Tecflare</strong> Multisite</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-  
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-<?php
-if ($neededpartner) {
-    if (!isset($_SESSION["nonpartner"]))
-    {
-        ?>
-         <ol class="breadcrumb">
-  <li>Installer</li>
-  <li>Step Partners Only</li>
-</ol>
-<div class="page-header">
-  <h1>Step 0 <small>Partners Only</small></h1>
-</div>
-<div class="container">
-    <p>
-        <?php
-        if ($_GET["error"] == "aq")
-{
-  ?>
- <div class="alert alert-danger" role="alert">
-  <strong>Error!</strong> Partner id not correct.
-</div>
-<?php
-}
-?>
-        Please enter your partner id.
-    </p>
-    <form method="POST" action="chk.php">
-     <div class="input-group">
-  <span class="input-group-addon" id="basic-addon1">ID:</span>
-
-  <input type="text" name="partner" class="form-control" placeholder="admin" required="true" aria-describedby="basic-addon1">
-</div>
-<input type="submit" class="btn btn-info" value="Next" role="button">
-</form>
-        <?php
-        die();
-    }
-}
-if ($_GET["page"] == 1)
-{
-  ?>
-<ol class="breadcrumb">
-  <li>Installer</li>
-  <li>Step 1</li>
-</ol>
-<div class="page-header">
-  <h1>Step 1 <small>Welcome to Setup</small></h1>
-</div>
-<div class="container">
-    <p>
-    Welcome to the new Multisite. Multisite 4 is the latest version. It comes with new unique features. Multisite has also changed the style because there were some issues. Press next to experience the new multisite interface and website system.
-
-</p>
-    <a href="index.php?page=aa" class="btn btn-info" role="button">Next</a>
-    </div>
-</div>
-<?php
-}
-elseif ($_GET["page"] == "aa")
-{
-  echo '
-<ol class="breadcrumb">
-  <li>Installer</li>
-  <li>Step 1a</li>
-</ol>
-<div class="page-header">
-  <h1>Step 1a <small>Requirements</small></h1>
-</div>
-<div class="container">
- <p>
-  <div class="alert alert-info">
-  ';
-  $version = phpversion();
-  if (!$version > "4.0.5")
-  {
-    die("PHP version must be above 4.0.5.");
-  }
-  if (file_exists("../config.php"))
-  {
-    die("System already installed");
-  }
-
-    echo "System meets requirements.";
-  
-echo '
- </p>
+<div class="app-bar navy"><div class="app-bar-element">Poke Installer</div></div>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-3">poKe New Installer</h1>
+    <p class="lead">Welcome to the poKe!</p>
   </div>
-    <a href="index.php?page=2" class="btn btn-info" role="button">Next</a>
-    </div>
-</div>';
-}
-elseif($_GET["page"] == 2)
-{
-  echo '
-  <ol class="breadcrumb">
-  <li>Installer</li>
-  <li>Step 2</li>
-</ol>
-<div class="page-header">
-  <h1>Step 2 <small>Database Info</small></h1>
-</div>
-<div class="container">
-    <p>
-    Please enter your MYSQL credentials to save your websites data. We need a database because this system will not save code into files as that is unsecure.
-    </p>
-    <div class="alert alert-waning" role="alert">
-  <strong>Waning!</strong> Database will be edited.
-</div>
-';
-if (isset($_GET["error"]) && $_GET["error"] == 1)
-{
-  ?>
- <div class="alert alert-danger" role="alert">
-  <strong>Error!</strong> Cannot connect to database or there is a problem with the database.
 </div>
 <?php
-}
-?>
-    <form method="POST" action="import.php">
-    <div class="input-group">
-  <span class="input-group-addon" id="basic-addon1">Hostname</span>
-  <input type="text" name="hostname" class="form-control" placeholder="Localhost" aria-describedby="basic-addon1">
-</div>
- <div class="input-group">
-  <span class="input-group-addon" id="basic-addon1">Usename</span>
-  <input type="text" name="usename" class="form-control" placeholder="root" aria-describedby="basic-addon1">
-</div>
- <div class="input-group">
-  <span class="input-group-addon" id="basic-addon1">Password</span>
-  <input type="password" name="password" class="form-control" placeholder="" aria-describedby="basic-addon1">
-</div>
- <div class="input-group">
-  <span class="input-group-addon" id="basic-addon1">Database</span>
-  <input type="text" name="database" class="form-control" placeholder="" aria-describedby="basic-addon1">
-</div>
-         <a href="index.php?page=1" class="btn btn-info" role="button">Back</a>
-    <input type="submit" class="btn btn-info" value="Next" role="button">
-    </form>
-    </div>
-</div>
-  <?php
-}
-elseif ($_GET["page"] == 3 && file_exists("../config.php"))
-{
- ?>
-   <ol class="breadcrumb">
-  <li>Installer</li>
-  <li>Step 3</li>
-</ol>
-<div class="page-header">
-  <h1>Step 3 <small>Admin Account</small></h1>
-</div>
-<div class="container">
-    <p>
-        <?php
-        if (isset($_GET["error"]) &&$_GET["error"] == 9)
-{
-  ?>
- <div class="alert alert-danger" role="alert">
-  <strong>Error!</strong> Password must contain a symbol, Uppercase/Lowercase letter, and number.
-</div>
-<?php
-}
-?>
-        Now we need to create you an admin account so you can use our system. Please make sure that you can remember the usename or password.
-    </p>
-    <form method="POST" action="import.php">
-     <div class="input-group">
-  <span class="input-group-addon" id="basic-addon1">Usename</span>
-  <input type="hidden" name="type" value="yes">
-  <input type="text" name="usename" class="form-control" placeholder="admin" required="true" aria-describedby="basic-addon1">
-</div>
-    <div class="input-group">
-  <span class="input-group-addon" id="basic-addon1">Password</span>
-  <input type="password" name="password" class="form-control" placeholder="admin" required="true" aria-describedby="basic-addon1">
-</div>
-<input type="submit" class="btn btn-info" value="Next" role="button">
-</form>
- <?php
-}
-elseif($_GET["page"] == 4 && file_exists("../config.php"))
-{
+if (!isset($_GET['install'])) {
     ?>
-      <ol class="breadcrumb">
-  <li>Installer</li>
-  <li>Step 3</li>
-</ol>
-<div class="page-header">
-  <h1>Step 4 <small>Thank You</small></h1>
+<div class="alert alert-info" role="alert">
+  <strong>Welcome</strong> Thank you for installing poKe. To install please press install.
 </div>
-<div class="container">
-    <p>
-        Your account has now been created. To login please go to http://website-here.com/admin/. Make
-        sure that you delete the install folder.
-    </p>
-    <?php
-    
+     <?php
+        if (isset($_GET['error'])) {
+            ?>
+          <div class="alert alert-warning alert-dismissible fade in" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+  <strong>Holy guacamole!</strong> Cannot Connect to MySQL.
+
+</div>
+
+          <?php
+
+        }
+    ?>
+      
+
+<!-- Modal -->
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+
+        <h4 class="modal-title" id="myModalLabel">Database Credentials</h4>
+      </div>
+      <div class="modal-body">
+   
+        <form method="Post" action="database.php" data-role="validator">
+           <fieldset class="form-group">
+    <label>MySQL Hostname:</label>
+    <input type="text" required="true" name="hostname" class="form-control" id="" placeholder="localhost">
+  
+  </fieldset>
+        <fieldset class="form-group">
+    <label>Username:</label>
+    <input type="text" required="true" name="username" class="form-control" id="" placeholder="root">
+  </fieldset>
+   <fieldset class="form-group">
+    <label>Password:</label>
+    <input type="password" required="true" name="password" class="form-control" id="" placeholder="password">
+  </fieldset>
+  <fieldset class="form-group">
+    <label>Database:</label>
+    <input type="text" required="true" name="database" class="form-control" id="" placeholder="data">
+  </fieldset>
+
+  <div class="modal-dialog" role="document" style="width: 100%;">
+    <div class="modal-content">
+      <div class="modal-header">
+            <fieldset class="form-group">
+    <label>Poke Username:</label>
+    <input type="text" required="true" name="username_p" class="form-control" id="" placeholder="Username">
+  </fieldset>
+   <fieldset class="form-group">
+    <label>Poke Password:</label>
+    <input type="password" required="true" name="password_p" class="form-control" id="myPassword" placeholder="Password">
+
+
+  </fieldset>
+        </div>
+        </div>
+        </div>
+<Br>
+  <button type="submit" class="btn btn-primary" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Submit</button>
+          </form>
+      </div>
+      <div class="modal-footer">
+      
+      </div>
+    </div>
+  </div>
+
+ 
+ <?php
+
 } else {
-  header("Location: ?page=1");
+    ?>
+<div class="alert alert-success" role="alert">
+  <strong>Well done!</strong> You successfully installed to poKe System.
+</div>
+<center><ul class="list-group">
+  <li class="list-group-item">1. Remove install folder</li>
+  <li class="list-group-item">2. Login to the Administration Area</li>
+</ul></center>
+<?php
+
 }
 ?>
+<footer class="footer">
+        <p>&copy; Tecflare Corporation</p>
+      </footer>
 
-    <script src="javascript/bootstrap.min.js"></script>
-    <script src="javascript/jquery.js"></script>
+
+    <script src="js/bootstrap.js"></script>
+
   </body>
 </html>
-<?php
-}
-
-?>
