290ecf0607df3a94b1cf9fd91a9dd4a5d30a6a18
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2016-03-04 14:37:53 -0500

    Applied fixes from StyleCI

--- checkblk.php
+++ checkblk.php
@@ -1,12 +1,17 @@
 <?php
+
+/*
+ * Tecflare Corporation Property
+ */
+
 error_reporting(0);
-include("config.php");
+include 'config.php';
 $conn = new mysqli($hostname, $usename, $password, $database);
 $iptocheck = $_SERVER['REMOTE_ADDR'];
-if ($conn->connect_error) die();
-$query = mysqli_query($conn,"SELECT * FROM `Blockedips` WHERE blocked = '" . $iptocheck . "'");
-if(mysqli_num_rows($query) > 0){
-    header("Location: read.php?error=403");
+if ($conn->connect_error) {
+    die();
 }
-
-?>
+$query = mysqli_query($conn, "SELECT * FROM `Blockedips` WHERE blocked = '".$iptocheck."'");
+if (mysqli_num_rows($query) > 0) {
+    header('Location: read.php?error=403');
+}
