0fe71dbdd2c597d55d6c2e95e27804d397295e0a
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2016-03-04 18:58:47 -0700

    Update preinst.sh

--- test/preinst.sh
+++ test/preinst.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo apt-get install -qq -y apache2 php5 mysql-server
 mysqladmin -u root password test
+mysql -u root -p 'test' -e 'CREATE DATABASE test'
+# database should be installed now run the installer
+curl localhost/install/database.php?test
