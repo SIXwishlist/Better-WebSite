a6ff55e68b7904f872775c53c9b6f9af65700843
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2016-03-04 19:03:30 -0700

    Update preinst.sh

--- test/preinst.sh
+++ test/preinst.sh
@@ -3,6 +3,6 @@ sudo apt-get install -qq -y apache2 php5 mysql-server
 mysqladmin -u root password test
 mysql -u root -p 'test' -e 'CREATE DATABASE test'
 # database should be installed now run the installer
-curl localhost/install/database.php?test
+php install.php
 #End of installation
 echo "Successfully installed the database files"
