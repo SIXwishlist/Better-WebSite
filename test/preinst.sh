3b17c465c9ecc41ef7bca4a2c87d9094fbf48180
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2016-03-05 11:50:44 -0600

    Update preinst.sh

--- test/preinst.sh
+++ test/preinst.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mysqladmin -u root password test
 # database should be installed now run the installer
+cd test
 php install.php
 #End of installation
 echo "Successfully installed the database files"
