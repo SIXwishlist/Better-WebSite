995340a57663019d6200d36b659c97e07b967ccb
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2016-03-05 11:13:09 -0600

    Update preinst.sh

--- test/preinst.sh
+++ test/preinst.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-sudo apt-get purge mysql-server 
-sudo apt-get install mysql-server 
 mysqladmin -u root password test
-mysql -u root -p 'test' -e 'CREATE DATABASE test'
 # database should be installed now run the installer
 php install.php
 #End of installation
