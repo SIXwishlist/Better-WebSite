251e2fb4989d8f0244b83140edae14deecf3637f
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2016-03-04 19:12:09 -0700

    Update preinst.sh

--- test/preinst.sh
+++ test/preinst.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+type mysql >/dev/null 2>&1 && sudo apt-get purge apache2 php5 mysql-server || echo "Warning MySQL not present. This is OK!"
 sudo apt-get install -qq -y apache2 php5 mysql-server
 mysqladmin -u root password test
 mysql -u root -p 'test' -e 'CREATE DATABASE test'
